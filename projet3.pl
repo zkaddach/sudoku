5bceb3d430f3575c1ea4b1b8fb9317b4562fe3a9
zk3 <kaddachezakaria@gmail.com>
2018-06-09 17:22:25 +0200

    Valider Grille 3x3 Done

--- projet3.pl
+++ projet3.pl
@@ -1,7 +1,7 @@
 grille([
   1,2,3,4,5,6,7,8,2,
-  2,3,3,4,5,6,7,8,8,
-  3,2,4,5,5,8,5,8,6,
+  4,5,8,1,7,8,7,8,8,
+  9,7,6,2,3,9,5,8,6,
   4,2,3,4,5,6,6,8,8,
   5,2,2,5,7,6,6,8,7,
   6,2,3,5,5,7,7,8,6,
@@ -10,7 +10,7 @@ grille([
   9,2,3,4,5,6,6,7,8
   ]).
 
-numero([1,2,3,4,5,6,7,8,9]) :- !.
+numero([1,2,3,4,5,6,7,8,9,' ']) :- !.
 
 %Recupere l indice de la liste
 indice(L,C, N):- N is (L-1) * 9 + C-1.
@@ -65,12 +65,13 @@ recupererCol(G, NumCol, Res) :- recupererCol(G, NumCol, Res, 9, Tmp), !.
 recupererCol([TG|QG], NumCol, Res, Compteur, Tmp) :- Compteur > 0, Compteur1 is Compteur-1, indice(10-Compteur, NumCol, Indice), recupererElement([TG|QG], Indice, X), concat(Tmp, [X], Res1), recupererCol([TG|QG], NumCol, Res, Compteur1, Res1).
 recupererCol([TG|QG], _, Res, 0, Tmp) :- concat([], Tmp, Res), !.
 
-%Recuperer Grille 3x3 
-recupererGrille(NumGrille, Res) :- grille(G), indiceGrille(L,C,NumGrille), recupererLigne(G, L, L1), recupererLigne(G, L+1, L2), recupererLigne(G, L+2, L3), recupererGrille(L1,L2,L3,C,Res).
+%Recuperer Grille 3x3
+recupererGrille(NumGrille, Res) :- grille(G), indiceGrille(L,C,NumGrille), recupererLigne(G, L, L1), recupererLigne(G, L+1, L2), recupererLigne(G, L+2, L3), recupererGrille(L1,L2,L3,C,Res), !.
 recupererGrille(L1,L2,L3,C,Res) :- recupererGrille2(L1, C, Res1), recupererGrille2(L2, C, Res2), recupererGrille2(L3, C, Res3), concat(Res1, Res2, Res4), concat(Res4, Res3, Res).
 recupererGrille2(L, C, Res) :- C1 is C-1, C2 is C+1, recupererElement(L, C1, R1), recupererElement(L, C, R2), recupererElement(L, C2, R3), unifie(R1, R11), unifie(R2, R22), unifie(R3, R33), concat(R11, R22, R4), concat(R4, R33, Res).
 
 %Supprime Element
+supprimeElement(A, ' ', A):- !.
 supprimeElement(A, X, R) :- supprimeElement(A, X, L, R), !.
 supprimeElement([X|Q], X, L, R) :- concat(L, Q, R), !.
 supprimeElement([T|Q], X, L, R) :- concat(L, [T], C), supprimeElement(Q, X, C, R).
@@ -84,6 +85,9 @@ valideLigne(I, N, Ligne, NumPossible) :- recupererElement(Ligne, N, E), dansList
           N1 is N-1, valideLigne(I, N1, Ligne, B2).
 valideLigne(I, 0, Ligne, B):- !.
 
+valideLigne2(I, G):- N is 8, recupererLigne(G, I, Ligne), numero(NumPossible), recupererElement(Ligne, N, E),
+          dansListe(NumPossible, E), supprimeElement(NumPossible, E, B2), N1 is N-1, valideLigne(I, N1, Ligne, B2), !.
+
 %Valider une Colone
 valideCol(I) :- valideCol(I, 8), !.
 valideCol(I, N) :- grille(G), recupererCol(G, I, Col), numero(NumPossible), recupererElement(Col, N, E),
@@ -92,4 +96,7 @@ valideCol(I, N, Col, NumPossible) :- recupererElement(Col, N, E), dansListe(NumP
           N1 is N-1, valideCol(I, N1, Col, B2).
 valideCol(I, 0, Col, B):- !.
 
-%Valider Grille 3x3 TODO
+%Valider Grille 3x3 
+valideGrille(A2) :- recupererGrille(A2, R), valideLigne2(1, R).
+
+%Valider Sudoku TODO
